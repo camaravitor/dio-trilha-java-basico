ed6550014a7be7f406ceea7ef84cac249252746d
camara.vitor@hotmail.com <camara.vitor@hotmail.com>
2023-09-04 18:53:50 -0300

    exemplos de declarações de var

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500; 
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //casting: alterado o tipo de variável para um que corresponda melhor
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero); // exemplo que mostra que uma variável pode ser alterada de acordo com o valor declarado
+
+        final double VALOR_DE_PI = 3.14; // exemplo que mostra que uma CONSTANTE (uso do final) não pode ter seu valor alterado
+
     }
 }
